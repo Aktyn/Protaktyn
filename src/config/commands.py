78f48f4393706b4a666bc35105669df302ed4869
Radosław Krajewski 😊 <Aktyn3@gmail.com>
2022-04-02 23:54:09 +0200

    hotfix

--- src/config/commands.py
+++ src/config/commands.py
@@ -7,7 +7,7 @@ class Commands:
     exit_current_module = r"(zako[nń]cz|wy[lł][aą]cz|zamknij)( (obecny|aktywny))? modu[łl]"
 
     class SCOREBOARD:
-        start_module = r"(za[lł]aduj|uruchom|w[lł][aą]cz|rozpocznij|zacznij) tablic[aąeęy] punkt[oó]w[aą]"
+        start_module = r"(za[lł]aduj|uruchom|w[lł][aą]cz|rozpocznij|zacznij) tablic[aąeęy] punkt[oó]w[aą]?"
         reset = r"(z?reset(uj)?|(wy)?zeruj) (punkty|wyniki|tablic[eę])"
         set_points = r".*(\s|^)([^\s]+)\s?(do|-)\s?([^\s]+).*"
         point_for_left_player = r"(punkt (dla|do|ma) lew(y|ego)|wygra[lł] lew[yo])"
